b2dda1b1e377b746370ee0d05223e604d94f2cb1
colinmfoster4723 <colin.foster4723@gmail.com>
2022-10-26 15:43:17 -0500

    Adjusted auth url

--- auth.config.js
+++ auth.config.js
@@ -1,16 +1,16 @@
 import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
 
-//DEV;
-const secret = JSON.parse(
-  require("child_process").execSync("node doppler_secrets.js")
-)["ADMIN_PRIVATE_KEY"];
-export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
-  privateKey: secret,
-  domain: "http://localhost:3000/",
-});
-
-// //PRODUCTION :
+// //DEV;
+// const secret = JSON.parse(
+//   require("child_process").execSync("node doppler_secrets.js")
+// )["ADMIN_PRIVATE_KEY"];
 // export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
-//   privateKey: process.env.ADMIN_PRIVATE_KEY,
-//   domain: "https://nft-verse.netlify.app",
+//   privateKey: secret,
+//   domain: "http://localhost:3000/",
 // });
+
+//PRODUCTION :
+export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
+  privateKey: process.env.ADMIN_PRIVATE_KEY,
+  domain: "https://nft-verse.netlify.app",
+});
